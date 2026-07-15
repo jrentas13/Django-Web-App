483487a519fddb08e8a665bcc20c52b21d7ea0a1
J-Rentas <javy.rentas@gmail.com>
2026-07-14 19:57:33 -0600

    Added images to site. Created Django form to handle contact. Site can now send a message to my personal gmail.

--- folio/views.py
+++ folio/views.py
@@ -1,13 +1,64 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.core.mail import send_mail
+
+from .forms import ContactForm
 
 
 def homepage(request):
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+
+        if form.is_valid():
+            name = form.cleaned_data["name"]
+            email = form.cleaned_data["email"]
+            subject = form.cleaned_data["subject"]
+            message = form.cleaned_data["message"]
+            full_message = f"""
+            You received a message from your portfolio website.
+
+            Name:
+            {name}
+
+            Email:
+            {email}
+
+            Message:
+            {message}
+            """
+
+            try:
+                send_mail(
+                    subject,
+                    full_message,
+                    email,
+                    ["javy.rentas@gmail.com"],
+                )
+                submitted = True
+
+            except Exception as e:
+                print(e)
+                submitted = False
+                
+
+            form = ContactForm()
+
+            submitted = True
+
+        else:
+            submitted = False
+        
+    else:
+        form = ContactForm()
+        submitted = False
+    
+
     projects = [
         {
             "title": "Calorie Tracker",
             "description": ("A Django web app that helps users track calories and meals."),
             "tools": ["Python", "Django", "HTML", "CSS", "JavaScript"],
+            "image": "folio/images/calorie-tracker.png",
             "github_url": "https://github.com/abitsma/Health_App",
             "live_url": ""
         },
@@ -15,6 +66,7 @@ def homepage(request):
             "title": "Portfolio Website",
             "description": ("A personal portfolio website built with Django"),
             "tools": ["Python", "Django", "Bootstrap"],
+            "image": "folio/images/portfolio.png",
             "github_url": "https://github.com/jrentas13/Django-Web-App",
             "live_url": ""
         },
@@ -23,6 +75,7 @@ def homepage(request):
             "description": ("A recipe sharing website designed to provide delicious and affordable"
             "recipes to college students."),
             "tools": ["JavaScript", "Node.js", "HTML", "CSS", "MySQL"],
+            "image": "folio/images/starving-college-students.png",
             "github_url": "https://github.com/jrentas13/Starving-College-Students",
             "live_url": ""
         }
@@ -34,6 +87,8 @@ def homepage(request):
         "introduction": ("I'm a Computer Engineering building projects to broaden my skillset"
         "in web development."),
         "projects": projects,
+        "form": form,
+        "submitted": submitted,
     }
 
     return render(request, "folio/homepage.html", context)
